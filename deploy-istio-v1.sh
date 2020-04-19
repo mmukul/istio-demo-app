3dfb60c830d6ed664abb2883a798066901686a8d
Mukul Malhotra <smilemukul2005@gmail.com>
2020-04-19 19:20:04 +0530

    modified istio setup script

--- deploy-istio-v1.sh
+++ deploy-istio-v1.sh
@@ -11,24 +11,3 @@ istioctl manifest apply --set profile=demo
 
 # Label to instruct Istio to automatically inject Envoy sidecar proxies
 kubectl label namespace default istio-injection=enabled
-
-# Deploy the Bookinfo sample application
-kubectl apply -f $HOME/bookapp.yaml
-
-# Check Services
-kubectl get services
-
-# Check pods
-kubectl get pods
-
-# Associate the application with Istio gateway to accessible from outside
-kubectl apply -f $HOME/ingress-gateway.yaml
-
-# Verify that the app is running inside the cluster and serving HTML pages by checking for the page title in the response
-kubectl exec -it $(kubectl get pod -l app=ratings -o jsonpath='{.items[0].metadata.name}') -c ratings -- curl productpage:9080/productpage | grep -o "<title>.*</title>"
-
-# Check the gateway
-kubectl get gateway
-
-# Determining the ingress details
-kubectl get svc istio-ingressgateway -n istio-system
