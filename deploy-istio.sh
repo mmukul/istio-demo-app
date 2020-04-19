ac183207df6796165511a033e8511022426fff0b
Mukul Malhotra <smilemukul2005@gmail.com>
2020-04-19 12:21:26 +0530

    added ingress gateway

--- deploy-istio.sh
+++ deploy-istio.sh
@@ -13,7 +13,7 @@ istioctl manifest apply --set profile=demo
 kubectl label namespace default istio-injection=enabled
 
 # Deploy the Bookinfo sample application
-kubectl apply -f book.yaml
+kubectl apply -f $HOME/book.yaml
 
 # Check Services
 kubectl get services
@@ -21,11 +21,14 @@ kubectl get services
 # Check pods
 kubectl get pods
 
+# Associate the application with Istio gateway to accessible from outside
+kubectl apply -f $HOME/book-gateway.yaml
+
 # Verify that the app is running inside the cluster and serving HTML pages by checking for the page title in the response
 kubectl exec -it $(kubectl get pod -l app=ratings -o jsonpath='{.items[0].metadata.name}') -c ratings -- curl productpage:9080/productpage | grep -o "<title>.*</title>"
 
-# Associate the application with Istio gateway to accessible from outside
-kubectl apply -f book-gateway.yaml
-
 # Check the gateway
 kubectl get gateway
+
+# Determining the ingress details
+kubectl get svc istio-ingressgateway -n istio-system
